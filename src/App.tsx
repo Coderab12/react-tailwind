5adbb5c7266ed6e3c9af88ce390f6b257c7aa639
coderab12 <bkcch1994@gmail.com>
2024-06-06 05:44:04 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from "react"
 
 function App() {
   return (
     <h1 className="text-3xl font-bold flex justify-center items-center">
-      Hello world
+      Hello world!
     </h1>
   )
 }
