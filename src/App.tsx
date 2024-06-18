4bd76ed8361950d0aa36c8957fa5b41fcbf3d736
parambrata chakrabarti <bkcch1994@gmail.com>
2024-06-18 04:31:59 +0000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return(
   <h1 className="text-3xl font-bold flex justify-center items-center">
     Welcome to my react, tailwind, typescript setup.
+    this is on my phone
   </h1>
 )
 }
